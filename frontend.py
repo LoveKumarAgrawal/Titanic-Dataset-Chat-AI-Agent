2734d095d01ae1f238ff0624ce1debc39e6c61dd
LoveKumarAgrawal <loveagrawal204@gmail.com>
2025-02-22 17:32:33 +0530

    added server url

--- frontend.py
+++ frontend.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # FastAPI endpoint URL
-API_URL = "http://127.0.0.1:8000/query/"
+API_URL = "https://titanic-dataset-chat-ai-agent.onrender.com/query/"
 
 # Title of the app with styling
 st.markdown(
